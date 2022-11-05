ae20b0f16e14ccea088cb5cf9e459be90c56fb09
Jacob Levinson <96364327+JacobLevinson@users.noreply.github.com>
2022-11-04 19:46:18 -0700

    Fixes to the the pages in the nav bar, hopefully it works now.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route }
+  from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
-import Account from './pages/account';
-import FindSmoothies from './pages/find_smoothies';
-import Favorites from './pages/favorites';
-  
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
+import Contact from './pages/contact';
+
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exact component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/account' component={Account} />
-        <Route path='/find_smoothies' component={FindSmoothies} />
-        <Route path='/favorites' component={Favorites} />
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='/sign-up' element={<SignUp />} />
       </Routes>
     </Router>
   );
 }
-  
+
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
-  
+
 const Home = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Right',
-        alignItems: 'Right',
-        height: '100vh'
-      }}
-    >
-      <h1>Welcome to GeeksforGeeks</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome to GeeksforGeeks</h1>
+        </div>
+    );
 };
-  
+
 export default Home;
