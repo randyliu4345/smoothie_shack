b850e92691d955682aafae7024562faa4a7a9760
Jacob Levinson <96364327+JacobLevinson@users.noreply.github.com>
2022-11-04 19:53:44 -0700

    Fixing the files that didnt get commited.

--- src/components/navbar/NavbarElements.js
+++ src/components/navbar/NavbarElements.js
@@ -1,14 +1,14 @@
-import { FaBars } from 'react-icons/fa';
-import { NavLink as Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { FaBars } from "react-icons/fa";
+import { NavLink as Link } from "react-router-dom";
+import styled from "styled-components";
 
 export const Nav = styled.nav`
-background: #63D471;
+background: #ffb3ff;
 height: 85px;
 display: flex;
 justify-content: space-between;
+padding: 0.2rem calc((100vw - 1000px) / 2);
 z-index: 12;
-
 `;
 
 export const NavLink = styled(Link)`
@@ -20,43 +20,34 @@ padding: 0 1rem;
 height: 100%;
 cursor: pointer;
 &.active {
-	color: #000000;
+	color: #4d4dff;
 }
 `;
 
 export const Bars = styled(FaBars)`
 display: none;
 color: #808080;
+@media screen and (max-width: 768px) {
+	display: block;
+	position: absolute;
+	top: 0;
+	right: 0;
+	transform: translate(-100%, 75%);
+	font-size: 1.8rem;
+	cursor: pointer;
+}
 `;
 
 export const NavMenu = styled.div`
 display: flex;
 align-items: center;
 margin-right: -24px;
-
-`;
-
-export const NavBtn = styled.nav`
-display: flex;
-align-items: center;
-margin-right: 24px;
-
-`;
-
-export const NavBtnLink = styled(Link)`
-border-radius: 4px;
-background: #808080;
-padding: 10px 22px;
-color: #000000;
-outline: none;
-border: none;
-cursor: pointer;
-transition: all 0.2s ease-in-out;
-text-decoration: none;
-
-margin-left: 24px;
-&:hover {
-	background: #fff;
-	color: #808080;
+/* Second Nav */
+/* margin-right: 24px; */
+/* Third Nav */
+/* width: 100vw;
+white-space: nowrap; */
+@media screen and (max-width: 768px) {
+	display: none;
 }
 `;
