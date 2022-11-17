c96966343628dc134cf92cd3fd1b2ecb9abd5435
randyliu4345 <66569039+randyliu4345@users.noreply.github.com>
2022-11-16 21:38:48 -0800

    Added user sign up

---
+++ backend/routes/Router.js
@@ -0,0 +1,105 @@
+const express = require("express");
+
+// Router is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /listings.
+const Router = express.Router();
+
+const dbo = require('../db/smoothie_db');
+
+// This section will help you get a list of all the documents.
+Router.route("/smoothie_shack").get(async function (req, res) {
+  const dbConnect = dbo.getDb();
+
+  dbConnect
+    .collection("smoothies")
+    .find({}).limit(50)
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching smoothies!");
+      } else {
+        res.json(result);
+      }
+    });
+});
+
+Router.route("/signup").post(function (req, res) {
+  const dbConnect = dbo.getDb();
+  const userInfo = {
+    user: req.body.user,
+    pass: req.body.pass
+  };
+
+  dbConnect
+    .collection("users")
+    .insertOne(userInfo, function (err, result) {
+      if (err) {
+        res.status(400).send("Error inserting user!");
+      } else {
+        console.log(`Added a new match with id ${result.insertedId}`);
+        res.status(204).send();
+      }
+    });
+});
+
+/*
+// This section will help you create a new document.
+Router.route("/listings/recordSwipe").post(function (req, res) {
+  const dbConnect = dbo.getDb();
+  const matchDocument = {
+    listing_id: req.body.id,
+    last_modified: new Date(),
+    session_id: req.body.session_id,
+    direction: req.body.direction
+  };
+
+  dbConnect
+    .collection("matches")
+    .insertOne(matchDocument, function (err, result) {
+      if (err) {
+        res.status(400).send("Error inserting matches!");
+      } else {
+        console.log(`Added a new match with id ${result.insertedId}`);
+        res.status(204).send();
+      }
+    });
+});
+
+// This section will help you update a document by id.
+Router.route("/listings/updateLike").post(function (req, res) {
+  const dbConnect = dbo.getDb();
+  const listingQuery = { _id: req.body.id };
+  const updates = {
+    $inc: {
+      likes: 1
+    }
+  };
+
+  dbConnect
+    .collection("listingsAndReviews")
+    .updateOne(listingQuery, updates, function (err, _result) {
+      if (err) {
+        res.status(400).send(`Error updating likes on listing with id ${listingQuery.id}!`);
+      } else {
+        console.log("1 document updated");
+      }
+    });
+});
+
+// This section will help you delete a record.
+Router.route("/listings/delete/:id").delete((req, res) => {
+  const dbConnect = dbo.getDb();
+  const listingQuery = { listing_id: req.body.id };
+
+  dbConnect
+    .collection("listingsAndReviews")
+    .deleteOne(listingQuery, function (err, _result) {
+      if (err) {
+        res.status(400).send(`Error deleting listing with id ${listingQuery.listing_id}!`);
+      } else {
+        console.log("1 document deleted");
+      }
+    });
+});
+*/
+module.exports = Router;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,9 +1,92 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const SignUp = () => {
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const [users, setUsers] = useState([]);
+    
+
+    const sendUser = (inUser, inPass) => {
+        axios
+            .post('http://localhost:5000/signUp', {user:inUser, pass:inPass})
+            .then((res) => {
+                console.log(res);
+                setUsers(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    // User Login info
+    const database = [
+        {
+            username: "user1",
+            password: "pass1"
+        },
+        {
+            username: "user2",
+            password: "pass2"
+        }
+    ];
+
+    const errors = {
+        uname: "username already taken"
+    };
+
+    const handleSubmit = (event) => {
+        //Prevent page reload
+        event.preventDefault();
+
+        var { uname, pass } = document.forms[0];
+
+        // Find user login info
+        sendUser(uname.value, pass.value);
+
+        // Compare user info
+        if (0) {
+            setErrorMessages({ name: "uname", message: errors.uname });
+        } else {
+            // Username not found
+            setIsSubmitted(true);
+        }
+    };
+
+    // Generate JSX code for error message
+    const renderErrorMessage = (name) =>
+        name === errorMessages.name && (
+            <div className="error">{errorMessages.message}</div>
+        );
+
+    // JSX code for login form
+    const renderForm = (
+        <div className="form">
+            <form onSubmit={handleSubmit}>
+                <div className="input-container">
+                    <label>Username </label>
+                    <input type="text" name="uname" required />
+                    {renderErrorMessage("uname")}
+                </div>
+                <div className="input-container">
+                    <label>Password </label>
+                    <input type="password" name="pass" required />
+                    {renderErrorMessage("pass")}
+                </div>
+                <div className="button-container">
+                    <input type="submit" />
+                </div>
+            </form>
+        </div>
+    );
+
     return (
-        <div>
-            <h1>Sign Up Successful</h1>
+        <div className="app">
+            <div className="login-form">
+                <div className="title">Sign Up</div>
+                {isSubmitted ? <div>You have successfully created an account</div> : renderForm}
+            </div>
         </div>
     );
 };
