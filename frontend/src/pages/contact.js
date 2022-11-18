57ddfb2eb3cd0f80c8c8160f3035f8b14a0fb10d
Jacob Levinson <96364327+JacobLevinson@users.noreply.github.com>
2022-11-17 17:22:15 -0800

    I added a proof of concept for obtaining the name of the logged in user while in another page. My example is on the contact us page, where we can see we say who is logged in currently.

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Contact = () => {
+    console.log("CONTACT US PAGE, USERS is :")
+    console.log(localStorage.users);
     return (
         <div>
             <h1>Mail us on feedback@geeksforgeeks.org</h1>
+            <div>{localStorage.users} is signed in btw</div>
         </div>
     );
 };
