d3fb573885716dd79eae832fedaee98eb715502a
dyang9 <danieldanyang@gmail.com>
2022-11-29 23:15:06 -0800

    Ported add/remove favorites to home

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -5,13 +5,14 @@ import './generalStyles.css'
 const Home = () => {
     const [smoothies, setSmoothies] = useState([]);
     const [query, setQuery] = useState(" ")
-
+    const [smoothies_states, setSmoothieStates] = useState({});
+    const [userid, setUserid] = useState("");
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const data = await axios.get(`http://localhost:5000/smoothie_shack/${query}`)
-                setSmoothies(data.data)
+                setSmoothies(data.data )
             } catch (error) {
                 console.error(error)
             }
@@ -19,6 +20,121 @@ const Home = () => {
 
         fetchData()
     }, [query])
+
+
+
+    useEffect(() => {
+        fetchSmoothies();
+        console.log("fetchSmoothies called from useEffect")
+    }, []);
+
+
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem("users");
+        console.log("useEffect called");
+        if (loggedInUser) {
+            setUserid(loggedInUser);
+        }
+        console.log("Called from useeffect: " + userid);
+        // console.log(typeof userid);
+        // console.log(userid.length);
+    }, [userid]);
+
+    
+
+    
+
+    const fetchSmoothies = () => {
+
+            var userfavs;
+            axios
+            .get('http://localhost:5000/users')
+            .then((res) => {
+                var users = Array.from(res.data);
+                for (let i = 0; i < users.length; i++) {
+                    if (users[i].user == localStorage.getItem("users")) {
+                        userfavs = Array.from(users[i].fav)
+                    }
+                }
+                return axios.get('http://localhost:5000/smoothie_shack')
+            })
+            .then((res) => {
+                // console.log(res.data);
+                var smoothiesArray = res.data;
+                var smoothiesMap = {};
+                smoothiesArray.forEach(smoothie => {
+                    smoothiesMap[smoothie.name] = false;
+                })
+                console.log(userfavs)
+                userfavs.forEach(fav => {
+                    smoothiesMap[fav] = true;
+                })
+                // console.log(smoothiesMap)
+                setSmoothieStates(smoothiesMap);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    const addOrRemoveFavorite = (smoothieName) => {
+        var smoothie_button = document.getElementById(smoothieName);
+        // console.log(smoothie_button)
+        // console.log(smoothie_button.innerHTML)
+        if (smoothie_button.innerHTML === "Add favorite")
+        {
+            smoothie_button.innerHTML = "Remove favorite";
+            axios.put('http://localhost:5000/smoothie_shack/addFavorite', {id : userid, fav : smoothieName});
+            axios.put('http://localhost:5000/smoothie_shack/updateSmoothieFreq', {smoothie : smoothieName, count : 1})
+        }
+
+        else
+        {
+            smoothie_button.innerHTML = "Add favorite";
+            axios.put('http://localhost:5000/smoothie_shack/removeFavorite', {id : userid, fav : smoothieName});
+            axios.put('http://localhost:5000/smoothie_shack/updateSmoothieFreq', {smoothie : smoothieName, count : -1})
+        }
+
+        
+
+    }
+    if (userid.length === 0) {
+        return (
+            <>
+                <h1>Find your next favorite smoothie!</h1>
+                <br></br>
+                <div className="search">
+                    <input type="text"
+                           placeholder={"Search Name or Ingredient"}
+                           className={"input"}
+                           onChange={event => setQuery(event.target.value)}
+                           value={query}
+                    />
+                </div>
+    
+                <div className='item-container'>
+                {Array.isArray(smoothies)
+                    ?
+                    smoothies.map((smoothie) => (
+                        <div className='card'>
+                            <body>
+                            <h2>Name: {smoothie.name}</h2>
+                            <h3>Ingredients: {smoothie.ingredients}</h3>
+                            <h3>Calories: {smoothie.calories}</h3>
+                            <h3>Protein(g): {smoothie.protein}</h3>
+                                <br></br>
+                            </body>
+                        </div>
+                    )) : null}
+                </div>
+                
+            </>
+            
+        );
+    }
+
+
+
 
     return (
         <>
@@ -39,6 +155,7 @@ const Home = () => {
                 smoothies.map((smoothie) => (
                     <div className='card'>
                         <body>
+                        <h3> <button id={smoothie.name} onClick={() => {addOrRemoveFavorite(smoothie.name);}}>{smoothies_states[smoothie.name] ? "Remove favorite" : "Add favorite"}</button></h3>
                         <h2>Name: {smoothie.name}</h2>
                         <h3>Ingredients: {smoothie.ingredients}</h3>
                         <h3>Calories: {smoothie.calories}</h3>
